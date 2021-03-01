483ac6124b139b47f1489aeb1e7285247e44054f
Van Lao <vanlaodev@gmail.com>
2021-03-01 17:42:50 +0800

    update edit inventory form ui

--- inventory-frontend/src/views/EditInventoryForm.vue
+++ inventory-frontend/src/views/EditInventoryForm.vue
@@ -1,53 +1,26 @@
 <template>
   <div class="container-fluid">
     <b-card :title="$t('editInventoryForm')">
-      <b-form @submit.prevent="savedata" class="mt-4">
-        <div class="form-group">
-          <div class="row">
-            <div class="col-sm-12 col-md-1">
-              <label>{{ $t('item') }}:</label>
-            </div>
-            <div class="col-sm-12 col-md-11">
-              <input type="text" required="required" class="form-control" id="title" />
-            </div>
-          </div>
-        </div>
-
-        <div class="form-group">
-          <div class="row">
-            <div class="col-sm-12 col-md-1">
-              <label>{{ $t('department') }}:</label>
-            </div>
-            <div class="col-sm-12 col-md-11">
-              <select class="form-control" v-model="depselected">
-                <option
-                  v-bind:key="dep.value"
-                  v-for="dep in deplist"
-                  :value="dep.value"
-                >{{dep.text}}</option>
-              </select>
-            </div>
-          </div>
-        </div>
-
-        <div class="form-group">
-          <div class="row">
-            <div class="col-sm-12 col-md-1">
-              <label>{{ $t('from') }}:</label>
-            </div>
-            <div class="col-sm-12 col-md-5">
-              <b-form-datepicker v-model="dtpFrom" :min="minDtpFrom" @input="onDtpFromUpdated"></b-form-datepicker>
-            </div>
-
-            <div class="col-sm-12 col-md-1">
-              <label>{{ $t('to') }}:</label>
-            </div>
-            <div class="col-sm-12 col-md-5">
-              <b-form-datepicker v-model="dtpTo" :min="minDtpTo"></b-form-datepicker>
-            </div>
-          </div>
-        </div>
-
+      <b-form @submit.prevent="savedata">
+        <b-form-group :label="`${$t('item')}:`" label-for="input-item">
+          <b-form-input id="input-item" v-model="item" required></b-form-input>
+        </b-form-group>
+        <b-form-group :label="`${$t('department')}:`" label-for="select-dept">
+          <select id="select-dept" class="form-control" v-model="depselected">
+            <option v-bind:key="dep.value" v-for="dep in deplist" :value="dep.value">{{dep.text}}</option>
+          </select>
+        </b-form-group>
+        <b-form-group :label="`${$t('from')}:`" label-for="dtp-from">
+          <b-form-datepicker
+            id="dtp-from"
+            v-model="dtpFrom"
+            :min="minDtpFrom"
+            @input="onDtpFromUpdated"
+          ></b-form-datepicker>
+        </b-form-group>
+        <b-form-group :label="`${$t('to')}:`" label-for="dtp-to">
+          <b-form-datepicker v-model="dtpTo" :min="minDtpTo"></b-form-datepicker>
+        </b-form-group>
         <b-button type="submit" variant="primary" class="mr-2">{{ $t('save') }}</b-button>
         <b-button @click.prevent="backToinquiry" variant="secondary">{{ $t('cancel') }}</b-button>
       </b-form>
@@ -61,6 +34,7 @@ import * as dayjs from "dayjs";
 export default {
   data() {
     return {
+      item: "",
       depselected: "ALL",
       dtpFrom: dayjs().format("YYYY-MM-DD"),
       dtpTo: dayjs().format("YYYY-MM-DD"),
